ddc7f95d93abdb4dbd41cc60e7976da1344da98f
singhkong7 <singhpriyansh2014@gmail.com>
2021-09-04 13:21:31 +0530

    Adding the project to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import Header from "./containers/Header";
+import ProductListing from './containers/ProductListing';
+import "./App.css";
+import ProductDetails from "./containers/ProductDetail";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/products/:productId" exact component={ProductDetails} />
+          <Route><h1>Error 404 Page Not Found</h1></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
